a141f6437e2204233687b078895de8c4a6abde9d
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-10-19 18:10:13 +0300

    adding the build menu logic

---
+++ 10_Asynchronous Programming/03_Task in C#/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Main method started...");
+        DoSomething();
+        Console.WriteLine("Main method ended.....");
+        Console.Read();
+    }
+
+    private static async void DoSomething()
+    {
+        Console.WriteLine("Some method started...");
+
+        await Wait();
+
+        Console.WriteLine("Some method ended...");
+    }
+
+    private static async Task Wait()
+    {
+        int seconds = 5;
+        await Task.Delay(TimeSpan.FromSeconds(seconds));
+        Console.WriteLine();
+        Console.WriteLine($"{seconds} seconds wait has ended");
+        Console.WriteLine();
+    }
+}
