243e241d622b0c37056bcc4d979e4be2e5ca6762
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-10-19 14:24:21 +0300

    Async and Await in C#
    https://dotnettutorials.net/lesson/async-and-await-operator-in-csharp/

---
+++ 10_Asynchronous Programming/02_Async and Await in C#/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Main method started...");
+        DoOtherMethod();
+        Console.WriteLine("Main method ended.....");
+        Console.ReadKey();
+    }
+
+
+    // example of an async method
+    private async void DoSomething()
+    {
+        // Do something
+
+        await Task.Delay(1000);
+
+        //Do other somethings
+    }
+
+
+    private async static void DoOtherMethod()
+    {
+        Console.WriteLine("Some method started...");
+        
+        // Thread.Sleep(TimeSpan.FromSeconds(5));
+        await Task.Delay(TimeSpan.FromSeconds(5));
+        
+        Console.WriteLine('\n');
+        Console.WriteLine("Some method ended......");
+    }
+}
