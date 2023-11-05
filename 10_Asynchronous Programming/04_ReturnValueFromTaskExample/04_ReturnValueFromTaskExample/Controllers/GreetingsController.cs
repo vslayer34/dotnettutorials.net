bf0b871bf7525d3d32d0d06fe5abfc1af43b2400
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-11-05 10:29:08 +0200

    Added the task return type example

---
+++ 10_Asynchronous Programming/04_ReturnValueFromTaskExample/04_ReturnValueFromTaskExample/Controllers/GreetingsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _04_ReturnValueFromTaskExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingsController : ControllerBase
+    {
+        // api/greetings/name
+        [HttpGet("{name}")]
+        public string GetGreetings(string name) => $"Hello {name}, Welcone to web API";
+    }
+}
---
+++ 10_Asynchronous Programming/04_ReturnValueFromTaskExample/ConsoleTestAPIApplication/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Linq;
+
+namespace ConsoleTestAPIApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Started.....");
+            Console.Write("Enter the name: ");
+            string name = Console.ReadLine();
+
+            SomeMethod(name);
+
+            Console.WriteLine("Main Method Ended.....");
+            Console.ReadKey();
+        }
+
+
+        public async static void SomeMethod(string name)
+        {
+            Console.WriteLine("SomeMethod Started...");
+            string greetingMessage = await Greetings(name);
+
+            Console.WriteLine();
+            Console.WriteLine($"{greetingMessage}");
+            Console.WriteLine("SomeMethod Ended...");
+        }
+
+
+        public static async Task<string> Greetings(string name)
+        {
+            string message = string.Empty;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5068/");
+
+                HttpResponseMessage response = await client.GetAsync($"api/greetings/{name}");
+                message = await response.Content.ReadAsStringAsync();
+            }
+
+            return message;
+        }
+    }
+}
