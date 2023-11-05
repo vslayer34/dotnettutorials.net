9770058ffca55185b0f69ac3d0446aa2c786374d
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-11-05 12:12:08 +0200

    04_How to Return a Value from Task in C#
    https://dotnettutorials.net/lesson/return-a-value-from-a-task-in-csharp/

--- 10_Asynchronous Programming/04_ReturnValueFromTaskExample/ConsoleTestAPIApplication/Program.cs
+++ 10_Asynchronous Programming/04_ReturnValueFromTaskExample/ConsoleTestAPIApplication/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleTestAPIApplication
             Console.Write("Enter the name: ");
             string name = Console.ReadLine();
 
-            SomeMethod(name);
+            // SomeMethod(name);
+            SomeMthod2();
 
             Console.WriteLine("Main Method Ended.....");
             Console.ReadKey();
@@ -20,10 +21,17 @@ namespace ConsoleTestAPIApplication
         public async static void SomeMethod(string name)
         {
             Console.WriteLine("SomeMethod Started...");
-            string greetingMessage = await Greetings(name);
-
-            Console.WriteLine();
-            Console.WriteLine($"{greetingMessage}");
+            try
+            {
+                string greetingMessage = await Greetings(name);
+                Console.WriteLine();
+                Console.WriteLine($"{greetingMessage}");
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Error occured: {ex.Message}");
+            }
             Console.WriteLine("SomeMethod Ended...");
         }
 
@@ -36,11 +44,41 @@ namespace ConsoleTestAPIApplication
             {
                 client.BaseAddress = new Uri("http://localhost:5068/");
 
-                HttpResponseMessage response = await client.GetAsync($"api/greetings/{name}");
+                HttpResponseMessage response = await client.GetAsync($"api/greetings2/{name}");
+                response.EnsureSuccessStatusCode();
+
                 message = await response.Content.ReadAsStringAsync();
             }
 
             return message;
         }
+
+        //-----------------------------------------------------------------------------------------
+        //---------------------------------return complex types------------------------------------
+
+        private static async void SomeMthod2()
+        {
+            Employee emp = await GetEmployeeDetails();
+            Console.WriteLine($"ID: {emp.Id.ToString().PadRight(20)}Name: {emp.Name.PadRight(20)}\tSalary: {emp.Salary}");
+        }
+
+        private static async Task<Employee> GetEmployeeDetails()
+        {
+            Employee employee = new()
+            {
+                Id = 101,
+                Name = "Jamaes",
+                Salary = 10000m
+            };
+
+            return employee;
+        }
+    }
+
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set ;} = string.Empty;
+        public decimal Salary { get; set; }
     }
 }
