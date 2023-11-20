6f6bcdd4d38843f5ac69e0baca3df7cf7719fac9
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-11-21 01:29:20 +0200

    added How to Cancel a task in C#

---
+++ 10_Asynchronous Programming/07_How to Cancel a Task in C# using Cancellation Token/07_How to Cancel a Task in C# using Cancellation Token/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _07_How_to_Cancel_a_Task_in_C__using_Cancellation_Token;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        CancellationTokenSource cancellationTokenSource = new();
+
+        cancellationTokenSource.CancelAfter(5000);
+
+
+        try
+        {
+            await LongRunningTask(cancellationTokenSource.Token);
+        }
+        catch (TaskCanceledException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public static async Task LongRunningTask(CancellationToken token)
+    {
+        while (true)
+        {
+            // do someting
+
+            if (token.IsCancellationRequested)
+            {
+                throw new TaskCanceledException();
+            }
+        }
+    }
+}
+
