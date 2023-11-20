65f1c0807d2107fdc63ad7b3cc5b94ec0b4991b8
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-11-21 01:11:14 +0200

    How to Limit Number of Concurrent Tasks in C#
    https://dotnettutorials.net/lesson/how-to-limit-number-of-concurrent-tasks-in-csharp/

--- 10_Asynchronous Programming/06_How to Limit Number of Concurrent Tasks in C#/Program.cs
+++ 10_Asynchronous Programming/06_How to Limit Number of Concurrent Tasks in C#/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 
 
 class Program
 {
+    // allow a maximum of 3 tasks to be excuted at a time
+    private static SemaphoreSlim semaphoreSlim = new(3);
     private static void Main(string[] args)
     {
         Stopwatch stopwatch = new();
         stopwatch.Start();
         Console.WriteLine("Main Thread started");
 
-        List<CreditCard> creditCards = CreditCard.GenerateCreditCards(1000000);
+        List<CreditCard> creditCards = CreditCard.GenerateCreditCards(15);
         Console.WriteLine($"Number of generated credit cards: {creditCards.Count}");
 
         ProcessCreditCards(creditCards);
@@ -21,7 +25,7 @@ class Program
         Console.ReadKey();
     }
 
-
+    /*
     public static async void ProcessCreditCards(List<CreditCard> creditCards)
     {
         Stopwatch stopwatch = new();
@@ -40,6 +44,49 @@ class Program
 
         //It will execute all the tasks concurrently
         await Task.WhenAll(tasks);
+
+        stopwatch.Stop();
+        Console.WriteLine($"processing of {creditCards.Count} credit cards done in {stopwatch.ElapsedMilliseconds / 1000.0} seconds");
+    }
+    */
+
+    public static async void ProcessCreditCards(List<CreditCard> creditCards)
+    {
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+
+        List<Task<string>> tasks = new();
+
+        // We need to use async lamda expression
+        tasks = creditCards.Select(async card =>
+        {
+            // this will tell if we have more than 3 tasks running
+            // and would wait untill the semaphore is released
+            await semaphoreSlim.WaitAsync();
+
+            // we need to use the wait operator as we're using semaphore WaitAsync
+            try
+            {
+                return await ProcessCard(card);
+            }
+            finally
+            {
+                // release the semaphore
+                semaphoreSlim?.Release();
+            }
+
+        }).ToList();
+
+        // It will execute all the tasks concurrently
+        //await Task.WhenAll(tasks);
+
+        // recieve the task response
+        string[] responses = await Task.WhenAll(tasks);
+
+        foreach (string response in responses)
+        {
+            Console.WriteLine(response);
+        }
 
         stopwatch.Stop();
         Console.WriteLine($"processing of {creditCards.Count} credit cards done in {stopwatch.ElapsedMilliseconds / 1000.0} seconds");
