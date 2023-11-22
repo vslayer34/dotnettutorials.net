57a5bd0788ad258b37ad183ad667300b985e67f7
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-11-22 16:50:20 +0200

    Started with Retry Pattern in C#

---
+++ 10_Asynchronous Programming/09_Retry Pattern in C#/TestConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+
+namespace TestConsoleApp;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Main method started.....");
+
+        RetryMethod();
+
+        Console.WriteLine("Main method ended.");
+        Console.ReadKey();
+    }
+
+
+    private static async void RetryMethod()
+    {
+        // it tells the number of times the connection should be retried
+        int retryTimes = 3;
+
+        // determine the wait time
+        int waitTime = 500;
+
+        for (int i = 0; i < retryTimes; i++)
+        {
+            try
+            {
+                Console.WriteLine("Starting operation....");
+                await RetryOperation();
+                Console.WriteLine("Operation Successful");
+                break;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Retry #{i + 1}: Getting Exception -> {ex.Message}");
+
+                // wait before trying again
+                await Task.Delay(waitTime);
+            }
+        }
+    }
+
+    private static async Task RetryOperation()
+    {
+        Console.WriteLine("Establishing conniction");
+        await Task.Delay(500);
+
+        throw new Exception("Exception Occurred in while Processing...\n");
+    }
+}
