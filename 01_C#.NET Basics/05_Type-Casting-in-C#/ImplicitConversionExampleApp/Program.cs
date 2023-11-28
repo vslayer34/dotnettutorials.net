f8e4c2a9a05383077f0fd2a19f6cdc994441d247
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-11-28 18:58:18 +0200

    Updated Type casting

---
+++ 01_C#.NET Basics/05_Type-Casting-in-C#/ImplicitConversionExampleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace ImplicitConversionExampleApp;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        int intNumber = 1500;
+
+        // Get type of intNumber
+        Type intNumberType = intNumber.GetType();
+
+        // Impilicit Conversion
+        double doubleNumber = intNumber;
+
+        // Get type of doubleNumber
+        Type doubleNumberType = doubleNumber.GetType();
+
+        Console.WriteLine("Value before conversion");
+        Console.WriteLine($"intNumber Value: {intNumber}");
+        Console.WriteLine($"intNumber Type: {intNumberType}");
+        Console.WriteLine($"int Size: {sizeof(int)} bytes");
+
+        Console.WriteLine(new string('-', 50));
+
+        Console.WriteLine("Value before conversion");
+        Console.WriteLine($"doubleNumber Value: {doubleNumber}");
+        Console.WriteLine($"doubleNumber Type: {doubleNumberType}");
+        Console.WriteLine($"double Size: {sizeof(double)} bytes");
+
+        Console.ReadKey();
+    }
+}
