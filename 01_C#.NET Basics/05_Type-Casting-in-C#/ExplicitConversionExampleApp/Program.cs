374c3bdc43938b177907ee9c87de193832ceea38
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-11-29 12:25:02 +0200

    /mnt/df4c678b-2df4-4fec-9aa4-9f939f62bfd5/Projects/CSharp/dotnettutorials.net/01_C#.NET Basics/05_Type-Casting-in-C#
    https://dotnettutorials.net/lesson/type-casting-in-csharp/

---
+++ 01_C#.NET Basics/05_Type-Casting-in-C#/ExplicitConversionExampleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace ExplicitConversionExampleApp;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        double numberDouble = 1.23;
+
+        // Explicit type casting
+        int numberInt = (int) numberDouble;
+
+        // Value before conversion
+        Console.WriteLine($"Original double value: {numberDouble}");;
+
+        // Value after conversion
+        Console.WriteLine($"Value after conversion: {numberInt}");
+
+        Console.WriteLine('\n' + "======================================================================" + '\n');
+
+        int intNum1 = 100;
+        byte byteNum1 = (byte) intNum1;                     // Explicit type casting
+        // Printing the original value and the converted value
+        Console.WriteLine($"Original Value: {intNum1}".PadRight(15) + $"Converted Value: {byteNum1}");
+
+        int intNum2 = 500;
+        byte byteNum2 = (byte) intNum2;                     // Explicit type casting
+        // Printing the original and converted value
+        Console.WriteLine($"Original Value: {intNum2}".PadRight(15) + $"Converted Value: {byteNum2}");
+
+        Console.ReadKey();
+    }
+}
---
+++ 01_C#.NET Basics/05_Type-Casting-in-C#/NonCompatibleTypeConversionExampleApp/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+namespace NonCompatibleTypeConversionExampleApp;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        string str = "100";
+        // int i1 = (int) str;                     // Compile error: Can't convert string to int
+
+        // Helper Conversion Methods
+        int i1 = Convert.ToInt32(str);      // Converting string to integer
+
+        double d = 123.45;
+        int i2 = Convert.ToInt32(d);        // Converting double to integer
+
+        float f = 45.678f;
+        string str2 = Convert.ToString(f);  // Converting float to string
+
+        Console.WriteLine($"Original value str: {str} and converted valie i1: {i1}");
+        Console.WriteLine($"Original value d: {d} and converted value i2: {i2}");
+        Console.WriteLine($"Original value f: {f} and converted value str2: {str2}");
+
+        Console.WriteLine();
+        Console.WriteLine("********************Parsing********************");
+        Console.WriteLine("===============================================");
+        Console.WriteLine();
+
+        {
+            string str11 = "100";
+            // Converting a string to int type
+            int i = int.Parse(str11);
+            Console.WriteLine($"Original string value: {str11} and the converted integer value: {i}");
+
+            string str22 = "TRUE";
+            // Converting a string to a boolean type
+            bool b = bool.Parse(str22);
+            Console.WriteLine($"Original string value: {str22} and the converted integer value: {b}");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("********************TryParse********************");
+        Console.WriteLine("================================================");
+        Console.WriteLine();
+
+        {
+            string str11 = "100";
+            bool isConverted11 = int.TryParse(str11, out int i11);
+
+            if (isConverted11)
+            {
+                Console.WriteLine($"Original string value str11: {str11} and the converted integer value i11: {i11}");
+            }
+            else
+            {
+                Console.WriteLine($"TryParse() failed to convert that string \"{str11}\" to an integer");
+            }
+
+            string str22 = "Rana";
+            bool isConverted22 = int.TryParse(str22, out int i22);
+
+            if (isConverted22)
+            {
+                Console.WriteLine($"Original string value str22: {str22} and the converted integer value i22: {i22}");
+            }
+            else
+            {
+                Console.WriteLine($"TryParse() failed to convert that string \"{str22}\" to an integer");
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
