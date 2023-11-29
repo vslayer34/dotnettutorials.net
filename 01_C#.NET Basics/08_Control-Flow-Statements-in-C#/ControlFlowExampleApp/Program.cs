e3c2789aad854ace89cc9b7eb8275bc4ec15e3da
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-11-29 18:41:48 +0200

    08_Control Flow Statements in C#
    https://dotnettutorials.net/lesson/control-flow-statements-in-csharp/

---
+++ 01_C#.NET Basics/08_Control-Flow-Statements-in-C#/ControlFlowExampleApp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+namespace ControlFlowExampleApp;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        // Normal Excution of programs
+        Console.WriteLine("**********Normal Execution**********");
+        Console.WriteLine("====================================");
+        {
+            int number = 1;
+            Console.WriteLine($"Executing Statement #{number++}");
+            Console.WriteLine($"Executing Statement #{number++}");
+            Console.WriteLine($"Executing Statement #{number++}");
+            Console.WriteLine($"Executing Statement #{number++}");
+            Console.WriteLine($"Executing Statement #{number++}");
+        }
+
+        Console.WriteLine('\n' + "===========================================================================" + '\n');
+        Console.WriteLine("**********if/else**********");
+        Console.WriteLine("===========================");
+
+        {
+            if (10 > 5)
+            {
+                Console.WriteLine("Executing Statement #1");
+                Console.WriteLine("Executing Statement #2");
+            }
+            else
+            {
+                Console.WriteLine("Executing Statement #3");
+                Console.WriteLine("Executing Statement #4");
+            }
+
+            Console.WriteLine("Executing Statement #5");
+        }
+
+        Console.WriteLine('\n' + "===========================================================================" + '\n');
+        Console.WriteLine("**********for loop**********");
+        Console.WriteLine("============================");
+
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Executing Statement #{i + 1}");
+            }
+
+            Console.WriteLine("Executing Final Statement");
+        }
+
+        Console.WriteLine('\n' + "===========================================================================" + '\n');
+        Console.WriteLine("**********goto control**********");
+        Console.WriteLine("================================");
+
+        {
+            Console.WriteLine("Executing Statement #1");
+
+            goto Rana;        // goto label someLabelName;
+            Console.WriteLine("Executing Statement #2");
+            Console.WriteLine("Executing Statement #3");
+            
+            Rana:             // label
+            Console.WriteLine("Executing Statement #4");
+            Console.WriteLine("Executing Statement #5");
+        }
+
+        
+
+        Console.ReadKey();
+    }
+}
