282b6d8f3c58792f33fb5a509dd35e06c1b3d332
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-12-04 14:11:32 +0200

    14_For Loop in C#
    https://dotnettutorials.net/lesson/for-loop-in-csharp/

--- 01_C#.NET Basics/14_For-Loop-in-C#/ForLoopExampleApp/Program.cs
+++ 01_C#.NET Basics/14_For-Loop-in-C#/ForLoopExampleApp/Program.cs
@@ -80,6 +80,110 @@ class Program
             }
         }
 
+        AddHeader("Armstrong Numbers");
+        // An Armstrong Number is a number that is equal to the sum of, the power of each digit by the total number of digits.
+        // For example, the numbers such as 0, 1, 153, 370, 371, 407, 1634, 8208, 9474 are Armstrong numbers.
+        // Let us have a look at the following diagram which shows how the Armstrong number is calculated.
+
+        // 371 = (3 * 3 * 3) + (7 * 7 * 7) + (1 * 1 * 1)
+        // 371 =      27     +     343     +      1
+
+        // 8208 = (8 * 8 * 8 * 8) + (2 * 2 * 2 * 2) + (0 * 0 * 0 * 0) + (8 * 8 * 8 * 8)
+        // 8208 =      4096       +        16       +        0        +       4096
+
+        {
+            int[] digitArray = new int[10];
+            int digitCount = 0;
+
+            // Get the input
+            Console.Write("Please enter the number: ");
+            int.TryParse(Console.ReadLine(), out int number);
+
+            int temp = number;
+            int i = 0;
+
+            while (temp > 0)
+            {
+                digitArray[i++] = temp % 10;
+                temp /= 10;
+                digitCount++;
+            }
+
+            double sum = 0;
+            for (int count = 0; count < digitCount; count++)
+            {
+                sum += MathF.Pow(digitArray[count], digitCount);
+            }
+
+
+            if (sum == number)
+            {
+                Console.WriteLine($"{number} is an Armstrong number");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not an Armstrong number");
+            }
+        }
+
+        AddHeader("Prime Number Example");
+
+        {
+            int number;
+            int i;
+
+            Console.Write("Please enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 2; i < number; i++)
+            {
+                if (number % i ==  0)
+                {
+                    break;
+                }
+            }
+
+            if (i == number && number >= 2)
+            {
+                Console.WriteLine($"{number} is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} isn't a prime number.");
+            }   
+        }
+
+        AddHeader("Fibonacci Series Example");
+
+        {
+            int number;
+            int number1 = 0;
+            int number2 = 1;
+            int temp;
+
+            Console.Write("Please enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number >= 1)
+            {
+                Console.Write($"{number1} {number2}");
+                temp = number1 + number2;
+
+                for (; temp <= number;)
+                {
+                    Console.Write($" {temp}");
+                    number1 = number2;
+                    number2 = temp;
+                    temp = number1 + number2;
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number greater than zero (0)");
+            }
+        }
+
         Console.ReadKey();
     }
 }
