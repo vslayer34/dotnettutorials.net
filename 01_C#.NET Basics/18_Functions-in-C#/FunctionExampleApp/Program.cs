53b11d2ae9bc9250749eca2d5a9fe5319f294f23
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-12-06 16:36:10 +0200

    18_Functions in C#
    https://dotnettutorials.net/lesson/functions-in-csharp/

---
+++ 01_C#.NET Basics/18_Functions-in-C#/FunctionExampleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace FunctionExampleApp;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("**********Built-in Function**********");
+        Console.WriteLine("=====================================");
+
+        {
+            int number = 25;
+            double squareRoot = Math.Sqrt(number);
+            Console.WriteLine($"Square root of {number} is: {squareRoot}");
+        }
+
+        Console.WriteLine('\n' + new string('=', 100) + '\n');
+
+        Console.ReadKey();
+    }
+
+    // example of user-defined functions
+    public int GetMax(int x, int y)
+    {
+        if (x > y)
+        {
+            return x;
+        }
+        else
+        {
+            return y;
+        }
+    }
+
+    private static int Add(int a, int b) => a + b;
+}
