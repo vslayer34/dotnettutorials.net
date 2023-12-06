b0217dbbc93f366099dfd6fd4898f6a78ff3c01f
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-12-06 14:45:28 +0200

    16_Continue Statement in C#
    https://dotnettutorials.net/lesson/continue-statement-in-csharp/

---
+++ 01_C#.NET Basics/16_Continue-Statement-in-C#/ContinueStatementExampleApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+
+namespace ContinueStatementExampleApp;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("**********continue Example**********");
+        Console.WriteLine("====================================");
+
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                if (i == 3)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"I: {i}");
+            }
+        }
+
+        Console.WriteLine('\n' + new string('=',100) + '\n');
+        Console.WriteLine("**********continue within Nested Loops**********");
+        Console.WriteLine("================================================");
+
+        {
+            for (int outer = 1; outer <= 5; outer++)
+            {
+                Console.WriteLine($"Outer: {outer}");
+
+                for (int inner = 1; inner <= 5; inner++)
+                {
+                    if (inner == outer)
+                    {
+                        continue;
+                    }
+
+                    Console.WriteLine($"=> {inner}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        Console.WriteLine('\n' + new string('=',100) + '\n');
+        Console.WriteLine("**********if statement insteat of continue (same example)**********");
+        Console.WriteLine("===================================================================");
+
+        {
+            for (int outer = 1; outer <= 5; outer++)
+            {
+                Console.WriteLine($"Outer: {outer}");
+
+                for (int inner = 1; inner <= 5; inner++)
+                {
+                    if (inner != outer)
+                    {
+                        Console.WriteLine($"=> {inner}");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
