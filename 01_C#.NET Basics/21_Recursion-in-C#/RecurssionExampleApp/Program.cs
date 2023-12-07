35bd0e518f43fccac0e8898ec5c2436cba4630f8
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-12-07 17:31:05 +0200

    Updated recursion in C#

---
+++ 01_C#.NET Basics/21_Recursion-in-C#/RecurssionExampleApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+
+namespace RecurssionExampleApp;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("**********Function Call Stack**********");
+        Console.WriteLine("=======================================");
+        
+        {
+            Console.WriteLine("Main Method Started");
+            Fun1(4);
+            Console.WriteLine("Main Method Ended");
+        }
+
+        Console.WriteLine('\n' + new string('=', 100) + '\n');
+        Console.WriteLine("**********Recurrsion Method**********");
+        Console.WriteLine("=====================================");
+
+        {
+            int x = 3;
+            PrintValue(x);
+        }
+
+        Console.WriteLine('\n' + new string('=', 100) + '\n');
+
+        Console.ReadKey();
+    }
+
+    private static void Fun1(int n)
+    {
+        Console.WriteLine("Fun1() Started");
+        Fun2(3);
+        Console.WriteLine("Fun2() Ended");
+    }
+
+    private static void Fun2(int n)
+    {
+        Console.WriteLine("Fun2() Started");
+        Fun3(2);
+        Console.WriteLine("Fun2() Ended");
+    }
+
+    private static void Fun3(int n)
+    {
+        Console.WriteLine("Fun3() Started");
+        Fun4(1);
+        Console.WriteLine("Fun3() Ended");
+    }
+
+    private static void Fun4(int n)
+    {
+        Console.WriteLine("Fun4() Started");
+        Console.WriteLine("Fun4() Ended");
+    }
+
+    private static void PrintValue(int n)
+    {
+        if (n > 0)
+        {
+            Console.Write($"{n} ");
+            PrintValue(n - 1);
+        }
+    }
+}
