a02a85f97dad7016c7a844cb739299cbaa1302ee
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-12-07 16:02:23 +0200

    20_Call By Value and Call By Reference in C#
    https://dotnettutorials.net/lesson/call-by-value-and-call-by-reference-in-csharp/

---
+++ 01_C#.NET Basics/20_Call-By-Value-and-Call-By-Reference-in-C#/DifferentCallTypesExampleApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+
+namespace DifferentCallTypesExampleApp;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("**********Call by Value**********");
+        Console.WriteLine("=================================");
+
+        {
+            int a = 15;
+            int b = a;
+            b = 30;
+
+            Console.WriteLine(a);
+        }
+
+        Console.WriteLine('\n' + new string('=', 100) + '\n');
+        Console.WriteLine("**********Copy Value in Methods**********");
+        Console.WriteLine("=========================================");
+
+        {
+            int a = 15;
+            UpdateValue(a);
+            Console.WriteLine(a);
+        }
+
+        Console.WriteLine('\n' + new string('=', 100) + '\n');
+        Console.WriteLine("**********Call by Value wit Reference Types**********");
+        Console.WriteLine("=====================================================");
+
+        {
+            Employee emp1 = new()
+            {
+                ID = 1001,
+                Name = "James"
+            };
+
+            Employee emp2 = emp1;
+            emp1.Name = "Smith";
+
+            Console.WriteLine($"emp1 name: {emp1.Name}");
+            Console.WriteLine($"emp2 name: {emp2.Name}");
+        }
+
+        Console.WriteLine('\n' + new string('=', 100) + '\n');
+        Console.WriteLine("**********Call by Reference with Value Types**********");
+        Console.WriteLine("======================================================");
+
+        {
+            int a = 15;
+            UpdateValueByRefernece(ref a);
+            Console.WriteLine(a);
+        }
+
+        Console.ReadKey();
+    }
+
+    private static void UpdateValue(int num)
+    {
+        num = 30;
+    }
+
+    private static void UpdateValueByRefernece(ref int a)
+    {
+        a = 30;
+    }
+}
+
+public class Employee
+{
+    public int ID { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
