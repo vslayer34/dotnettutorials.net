bb3ebceafffff590c2aa1111e542f6e6899aa58b
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-12-13 18:27:37 +0200

    23_Command Line Arguments in C#
    https://dotnettutorials.net/lesson/command-line-arguments-in-csharp/

---
+++ 01_C#.NET Basics/23_Command Line Arguments in C#/CommonLineArgumentsExampleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace CommonLineArgumentsExampleApp;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("**********Command Line Arguments in C#**********");
+        Console.WriteLine("================================================");
+
+        // passing dotnet run value1 value2 value3
+        {
+            Console.WriteLine($"First Command Line Argument {args[0]}");
+            Console.WriteLine($"Second Command Line Argument {args[1]}");
+            Console.WriteLine($"Third Command Line Argument {args[2]}");
+        }
+        
+        Console.WriteLine('\n' + new string('=', 70) + '\n');
+        Console.WriteLine("**********Passing Numeric Command Line Arguments**********");
+        Console.WriteLine("==========================================================");
+
+        // passing dotnet run 101 10.5
+        {
+            // convert into integer type
+            int argument1 = Convert.ToInt32(args[3]);
+            Console.WriteLine($"Argument in integer form: {argument1}");
+
+            // convert into double type
+            double argument2 = Convert.ToDouble(args[4]);
+            Console.WriteLine($"Argument in double form: {argument2}");
+        }
+        
+        Console.ReadKey();
+    }
+}
