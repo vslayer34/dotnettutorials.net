82a95ecc0ab52f643e00ea251245c26a3797f18e
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-12-23 23:07:33 +0200

    Why we Should Override ToString Method in C#
    https://dotnettutorials.net/lesson/why-we-should-override-the-tostring-method/

---
+++ 01_C#.NET Basics/29_Why we Should Override ToString Method in C#/OverrideToStringMethod/OverrideToStringMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace OverrideToStringMethod;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("**********ToString() Inherited Methods**********");
+        Console.WriteLine("================================================");
+        
+        {
+            int number = 10;
+            Console.WriteLine(number.ToString());
+        }
+
+        Console.WriteLine('\n' + new string('=', 70) + '\n');
+        Console.WriteLine("**********ToString() for classes**********");
+        Console.WriteLine("==========================================");
+
+        {
+            Employee emp = new();
+            emp.FirstName = "Azza";
+            emp.LastName = "Rana";
+
+            Console.WriteLine(emp.ToString());
+        }
+        // The above code would print: OverrideToStringMethod.Employee
+
+
+        Console.ReadKey();
+    }
+}
+
+public class Employee
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+
+    // Overriding the ToString() Method in C#:
+    public override string ToString() => $"{FirstName} {LastName}";
+}
