b1ed4a1c2e0ef0ad673b8aa0bddf373add1a8066
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-12-24 18:31:31 +0200

    31_Difference Between Convert.ToString and ToString Method in c#
    https://dotnettutorials.net/lesson/difference-between-convert-tostring-and-tostring-csharp/#google_vignette

---
+++ 01_C#.NET Basics/31_Difference Between Convert.ToString and ToString Method/ToStringVSConvertToStringDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ToStringVSConvertToStringDemo;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        // Both these methods are used to convert a value to a string. The difference is Convert.
+        // ToString() method handles null whereas the ToString() doesn’t handle null in C#.
+
+        Console.WriteLine("**********ToString() doesn't handle nulls**********");
+        Console.WriteLine("===================================================");
+        {
+            Customer c1 = null;
+            /*c1.ToString();*/
+        }
+
+        Console.WriteLine('\n' + new string('=', 70) + '\n');
+        Console.WriteLine("**********Convert.ToString() do handle nulls**********");
+        Console.WriteLine("======================================================");
+
+        {
+            Customer? c1 = null;
+            Convert.ToString(c1);
+
+            string? name = null;
+            Convert.ToString(name);
+
+            // The program runs and doesn't throw an exception
+        }
+
+        Console.ReadKey();
+    }
+}
+
+
+public class Customer
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
