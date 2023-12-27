8aebe1da3536aa66f0f5e450eef185b648b4b4fa
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-12-27 23:18:56 +0200

    02_Class and Objects in C#
    https://dotnettutorials.net/lesson/class-and-objects-csharp/

---
+++ 02_OOP in C#/02_Class and Objects in C#/ClassObjectDemo/ClassObjectDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ClassObjectDemo;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating object of the Calculator class
+        Calculator calculatorObj = new Calculator();
+
+
+        // Acessing Calculator class member using Calculator class object
+        int result = calculatorObj.CalculateSum(10, 20);
+        Console.WriteLine(result);
+    }
+}
+
+
+// Defining a class, blueprint or template
+public class Calculator
+{
+    public int CalculateSum(int num1, int num2) => num1 + num2;
+}
