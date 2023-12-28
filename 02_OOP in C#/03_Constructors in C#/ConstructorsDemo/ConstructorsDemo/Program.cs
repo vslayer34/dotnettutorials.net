11579d1393cb52859f65fbcab8e6e769fd084f4f
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-12-29 01:22:48 +0200

    03_Constructors in C#
    https://dotnettutorials.net/lesson/constructors-csharp/

---
+++ 02_OOP in C#/03_Constructors in C#/ConstructorsDemo/ConstructorsDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConstructorsDemo;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("**********Implicit Constructors**********");
+        Console.WriteLine("=========================================");
+
+        {
+            var test = new Test();
+            
+            Console.WriteLine(test.i);
+            Console.WriteLine(test.b);
+            Console.WriteLine(test.s ?? "test.s = null");
+        }
+
+        Console.WriteLine('\n' + new string('=', 70) + '\n');
+        Console.WriteLine("**********Explicit Constructors**********");
+        Console.WriteLine("=========================================");
+
+        {
+            ExplicitConstructor obj = new();
+        }
+
+        Console.WriteLine();
+    }
+}
+
+public class Test
+{
+    public int i;
+    public bool b;
+    public string s;
+}
+
+public class ExplicitConstructor
+{
+    public ExplicitConstructor()
+    {
+        Console.WriteLine("Explicit constructor is created");
+    }
+}
