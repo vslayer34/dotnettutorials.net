9c43d747aabcdc77593dbbfbb30029d99d8b8096
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-01-02 02:14:29 +0200

    08_Destructors in C#
    https://dotnettutorials.net/lesson/destructor-csharp/

---
+++ 02_OOP in C#/08_Destructors in C#/DestructorDemo/DestructorDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DestructorDemo;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("**********Destructor Example**********");
+        Console.WriteLine("======================================");
+
+        {
+            DestructorExample example1 = new DestructorExample();
+            DestructorExample example2 = new DestructorExample();
+
+            // Making example1 eligable for garbage collection
+            example1 = null;
+
+            GC.Collect();
+        }
+
+        Console.ReadKey();
+    }
+}
+
+
+class DestructorExample
+{
+    public DestructorExample()
+    {
+        Console.WriteLine("Constructor object created");
+    }
+
+    ~DestructorExample()
+    {
+        string type = GetType().Name;
+        Console.WriteLine($"object {type} is destroyed");
+    }
+}
