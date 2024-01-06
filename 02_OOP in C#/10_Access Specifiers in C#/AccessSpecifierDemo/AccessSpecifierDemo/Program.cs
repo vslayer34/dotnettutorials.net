a893d9f85df3b29b37e2056082de4dd5ebca9c6a
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-01-07 01:52:04 +0200

    Studying  10_AccessSpecifier in C#

--- 02_OOP in C#/10_Access Specifiers in C#/AccessSpecifierDemo/AccessSpecifierDemo/Program.cs
+++ 02_OOP in C#/10_Access Specifiers in C#/AccessSpecifierDemo/AccessSpecifierDemo/Program.cs
@@ -1,8 +1,39 @@
-﻿namespace AccessSpecifierDemo;
+﻿using AssemblyOne;
+
+
+namespace AccessSpecifierDemo;
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+    }
+}
+
+
+public class AnotherAssemblyClass1 : AssemblyOneClass1
+{
+    public void Display4()
+    {
+        // You cannot access the Private Member from the Derived Class from Other Assemblies
+        // Console.WriteLine(_id);              // Compile error
+
+        // We Can access the public Member from Derived Classes from Other Assemblies
+        Console.WriteLine(publicId);
+    }
+}
+
+
+public class AnotherAssemblyClass2
+{
+    public void Display33()
+    {
+        // You cannot access the Private Member from the Non-Derived Classes from Other Assemblies
+        AssemblyOneClass1 obj = new();
+
+        // Console.WriteLine(obj._id);          // Compile error
+
+        // We Can access the public Member from Non-Derived Classes from Other Assemblies
+        Console.WriteLine(obj.publicId);
     }
 }
---
+++ 02_OOP in C#/10_Access Specifiers in C#/AccessSpecifierDemo/AssemblyOne/AssemblyOneClass1.cs
@@ -0,0 +1,54 @@
+﻿namespace AssemblyOne;
+public class AssemblyOneClass1
+{
+    private int _id;
+    public int publicId;
+    protected int protectedId;
+
+    public void Display()
+    {
+        // Private Member Accessible with the Containing Type only Where they are created
+        // They are available only within that type
+        Console.WriteLine(_id);
+
+        // Public Members Accessible with the Containing Type where they are created
+        Console.WriteLine((publicId));
+
+        // protected Members Accessible with the Containing Type where they are created
+        Console.WriteLine(protectedId);
+    }
+}
+
+public class AssemblyOneClass2 : AssemblyOneClass1
+{
+    public void Display2()
+    {
+        // You cannot access the Private Member from the Derived Class within the Same Assembly
+
+        // Console.WriteLine(_id);     // Compile error
+
+        // We Can access public Members from Derived Class within the Same Assembly
+        Console.WriteLine(publicId);
+
+        // We Can access protected Member from Derived Classes within the Same Assembly
+        Console.WriteLine(protectedId);
+    }
+}
+
+
+public class AssemblyOneClass3
+{
+    public void Display3()
+    {
+        // You cannot access the Private Member from the Non-Derived Classes within the Same Assembly
+        
+        AssemblyOneClass1 obj = new();
+        // Console.WriteLine(obj._id);                      // Compile error
+
+        // We Can access public Members from Non-Derived Classes within the Same Assembly
+        Console.WriteLine(obj.publicId);
+
+        // We Cannot access protected Member from Non-Derived Classes within the Same Assembly
+        // Console.WriteLine(obj.protectedId);              // Compile error
+    }
+}
